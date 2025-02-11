039e6ae68084726c6413e462887a0fb4ed4cf214
dungs072 <82323846+dungs072@users.noreply.github.com>
2025-02-12 00:14:16 +0700

    Just initial steps for create path finding algorithm (A-122)

---
+++ Assets/Scripts/Base/AStarPathFinding/PathFinder.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+/*
+    * This class is responsible for finding the path between two points.
+    * It uses the A* algorithm to find the path. 
+ */
+public class PathFinder : MonoBehaviour
+{
+    
+}
