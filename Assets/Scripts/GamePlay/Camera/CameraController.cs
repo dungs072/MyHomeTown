20d6a2cee5d5dadf8a57a28b74c578cbaab8b4ec
dungs072 <82323846+dungs072@users.noreply.github.com>
2025-05-14 23:14:00 +0700

    Temporary finish logic for magic button (A-122)

--- Assets/Scripts/GamePlay/Camera/CameraController.cs
+++ Assets/Scripts/GamePlay/Camera/CameraController.cs
@@ -35,7 +35,7 @@ public class CameraController : MonoBehaviour
         mapWorld = ManagerSingleton.Instance.MapWorld;
     }
 
-    void Update()
+    void LateUpdate()
     {
         movementSpeed = GetMovementSpeed();
         UpdateInputOverTime();
