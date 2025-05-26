5a5569b84d70862a2819bd961e6321765b330313
dungs072 <82323846+dungs072@users.noreply.github.com>
2025-05-26 22:14:12 +0700

    update patrolling system (A-122)

--- Assets/BaseEngine/Scripts/Components/PatrollingSystem/PatrollingPath.cs
+++ Assets/BaseEngine/Scripts/Components/PatrollingSystem/PatrollingPath.cs
@@ -4,8 +4,8 @@ public class PatrollingPath : MonoBehaviour
 {
     [Header("Add them in order")]
     [SerializeField] private Transform[] waypoints;
-
-
+    [SerializeField] private string pathName = "DefaultPath";
+    public string PathName => pathName;
     void OnDrawGizmos()
     {
         foreach (var waypoint in waypoints)
--- Assets/BaseEngine/Scripts/Components/PatrollingSystem/PatrollingSystem.cs
+++ Assets/BaseEngine/Scripts/Components/PatrollingSystem/PatrollingSystem.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 public class PatrollingSystem : MonoBehaviour
 {
     [SerializeField] private List<PatrollingPath> patrollingPaths;
-
-     
+    private Dictionary<string, PatrollingPath> pathDictionary;
+    public IReadOnlyDictionary<string, PatrollingPath> PathDictionary => pathDictionary;
+    private void Awake()
+    {
+        pathDictionary = new Dictionary<string, PatrollingPath>();
+        foreach (var path in patrollingPaths)
+        {
+            if (path != null && !string.IsNullOrEmpty(path.name))
+            {
+                pathDictionary[path.name] = path;
+            }
+        }
+    }
 }
